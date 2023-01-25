4a2edd14260d84d799750b99c9a57a1c06f84d90
AmineBrd <aminebourada7@gmail.com>
2023-01-25 23:11:47 +0000

    data collection

--- src/Componenets/Road.jsx
+++ src/Componenets/Road.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import Feedback from './Feedback'
-
-function Road() {
+import './main.css'
+function Road(props) {
   return (
     <div>
-        
+        <h1 className='titrage'>Decouvrir {props.title}</h1>
+        <div className='container'>
+            <div className='line1'>
+            </div>
+            <div className='line2'></div>
+            <div className='line3'>
+            </div>
+        </div>
+        <div className='container'>
+            
+
+        </div>
     </div>
   )
 }
--- src/Pages/North.jsx
+++ src/Pages/North.jsx
@@ -3,11 +3,13 @@ import Nav from '../Componenets/Nav'
 import { Carousel } from 'flowbite'
 import Footer from '../Componenets/Footer'
 import Feedback from '../Componenets/Feedback'
+import Road from '../Componenets/Road'
 
 const North = () => {
   return (
     <div>
          <Nav />
+         <Road title={"Nord"} />
         <Feedback />
         <Footer />
     </div>
